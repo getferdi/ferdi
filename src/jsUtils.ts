a8b17bfafc832e4fb7cc9be5ddd10841b26a44d8
Vijay A <avijayr@protonmail.com>
2021-09-04 13:37:23 +0530

    ensure the return type is enforced

--- src/jsUtils.ts
+++ src/jsUtils.ts
@@ -1,3 +1,3 @@
 export const ifUndefinedString = (source: string | undefined, defaultValue: string): string => (source !== undefined ? source : defaultValue);
-export const ifUndefinedBoolean = (source: boolean | undefined, defaultValue: boolean): boolean => (source !== undefined ? source : defaultValue);
-export const ifUndefinedNumber = (source: number | undefined, defaultValue: number): number => (source !== undefined ? source : defaultValue);
+export const ifUndefinedBoolean = (source: boolean | undefined, defaultValue: boolean): boolean => Boolean(source !== undefined ? source : defaultValue);
+export const ifUndefinedNumber = (source: number | undefined, defaultValue: number): number => Number(source !== undefined ? source : defaultValue) || 0;
