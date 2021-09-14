5c8e961b809d1fb77056f46fb47f82c698f57ec7
mhatvan <markus_hatvan@aon.at>
2021-09-14 09:14:25 +0200

    moved spellcheck property to mainWindow.webPreferences

--- src/index.js
+++ src/index.js
@@ -213,6 +213,7 @@ const createWindow = () => {
     frame: isLinux,
     backgroundColor,
     webPreferences: {
+      spellcheck: retrieveSettingValue('enableSpellchecking'),
       nodeIntegration: true,
       contextIsolation: false,
       webviewTag: true,
